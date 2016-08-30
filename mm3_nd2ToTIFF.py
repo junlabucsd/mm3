8c8c3022e0cdceff1570b459bec57566686ac19d
jtsauls <saulsopolis@gmail.com>
2016-08-30 14:23:07 -0700

    removed datetime functions from nd2ToTIFF and put them in helpers

--- mm3_nd2ToTIFF.py
+++ mm3_nd2ToTIFF.py
@@ -49,45 +49,6 @@ if cmd_subfolder not in sys.path:
 
 import tifffile as tiff
 
-
-### Functions
-def date_to_jd(year,month,day):
-    if month == 1 or month == 2:
-        yearp = year - 1
-        monthp = month + 12
-    else:
-        yearp = year
-        monthp = month
-    # this checks where we are in relation to October 15, 1582, the beginning
-    # of the Gregorian calendar.
-    if ((year < 1582) or
-        (year == 1582 and month < 10) or
-        (year == 1582 and month == 10 and day < 15)):
-        # before start of Gregorian calendar
-        B = 0
-    else:
-        # after start of Gregorian calendar
-        A = math.trunc(yearp / 100.)
-        B = 2 - A + math.trunc(A / 4.)
-    if yearp < 0:
-        C = math.trunc((365.25 * yearp) - 0.75)
-    else:
-        C = math.trunc(365.25 * yearp)
-    D = math.trunc(30.6001 * (monthp + 1))
-    jd = B + C + D + day + 1720994.5
-    return jd
-
-def hmsm_to_days(hour=0, min=0, sec=0, micro=0):
-    days = sec + (micro / 1.e6)
-    days = min + (days / 60.)
-    days = hour + (days / 60.)
-    return days / 24.
-
-def datetime_to_jd(date):
-    days = date.day + hmsm_to_days(date.hour,date.minute,date.second,date.microsecond)
-    return date_to_jd(date.year, date.month, days)
-
-
 ### Main script
 if __name__ == "__main__":
 
