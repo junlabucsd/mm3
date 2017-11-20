bc2e1a1c1a7303bccd26c6757cb6128ce6006730
jtsauls <saulsopolis@gmail.com>
2017-11-20 13:10:00 -0800

    more cell cycle data to csv

--- code_matlab_GUI/cellcycle_mat2csv.m
+++ code_matlab_GUI/cellcycle_mat2csv.m
@@ -1,18 +1,21 @@
 % Export the array output data from FS stat_cell_param.m to .csv
 
 % directory from which to load and save data
-data_dir = '/Volumes/JunLabSSD_04/shift/ecoli/20171020_ecoli_shift14/analysis/cell_data/';
+data_dir = '../../analysis/cell_data/';
 
 % load output from stat_cell_param
 % load([data_dir 'cell_cycle_stat_GUI.mat']);
 
 % table header
 header = {'cell_id', 'initiation_time', 'initiation_length', ...
-          'termination_time', 'B', 'C', 'D', 'tau_cyc', 'width'}; 
+          'termination_time', 'B', 'C', 'D', 'tau_cyc', 'n_oc', 'width', ...
+          'birth_time', 'division_time', 'elong_rate', 'delta'}; 
 
 % make table
 T = table(cell_id', initiation_time_m', initiation_mass_m', ...
-          termination_time', B_period', C_period', D_period', tau_cyc', cell_width'); 
+          termination_time', B_period', C_period', D_period', tau_cyc', ...
+          n_oc', cell_width', birth_time', division_time', elongation_rate_fit', ...
+          added_length'); 
       
 % add header
 T.Properties.VariableNames = header;
