274ace693ff5ce6e99861a3386bd4ebba9a7859f
jtsauls <saulsopolis@gmail.com>
2019-03-05 19:17:52 -0800

    profile analysis fix
    Former-commit-id: 3be989df288b0456bb96cab9f5cff3bc7f302893

--- mm3_Subtract.py
+++ mm3_Subtract.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(prog='python mm3_Subtract.py',
                                      description='Subtract background from phase contrast and fluorescent channels.')
-    parser.add_argument('-f', '--paramfile',  type=file,
+    parser.add_argument('-f', '--paramfile',  type=str,
                         required=True, help='Yaml file containing parameters.')
     parser.add_argument('-o', '--fov',  type=str,
                         required=False, help='List of fields of view to analyze. Input "1", "1,2,3", or "1-10", etc.')
@@ -57,8 +57,8 @@ if __name__ == "__main__":
 
     # Load the project parameters file
     mm3.information('Loading experiment parameters.')
-    if namespace.paramfile.name:
-        param_file_path = namespace.paramfile.name
+    if namespace.paramfile:
+        param_file_path = namespace.paramfile
     else:
         mm3.warning('No param file specified. Using 100X template.')
         param_file_path = 'yaml_templates/params_SJ110_100X.yaml'
