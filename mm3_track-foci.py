4c12d9fca556b968230c08a083ec74715ba7ad08
Ryan Thiermann <ryan.thiermann@gmail.com>
2022-06-04 11:19:34 -0700

    Add cell cycle parameter calculation

--- mm3_track-foci.py
+++ mm3_track-foci.py
@@ -51,7 +51,7 @@ import mm3_plots as mm3_plots
 import mm3_GUI_helpers as GUI
 
 # set switches and parameters
-parser = argparse.ArgumentParser(prog='python mm3_nf.py',
+parser = argparse.ArgumentParser(prog='python mm3_track-foci.py',
                                  description='Finds foci.')
 parser.add_argument('-f', '--paramfile', type=str,
                     required=True, help='Yaml file containing parameters.')
@@ -98,13 +98,15 @@ times_all = np.array(times_all,np.int_)
 
 # make list of FOVs to process (keys of channel_mask file)
 fov_id_list = sorted([fov_id for fov_id in specs.keys()])
-fov_id_list = range(1,2,1)
+fov_id_list = range(1,30,1)
 ### foci analysis
 
 # create dictionary which organizes cells by fov and peak_id
 Cells_by_peak = mm3_plots.organize_cells_by_channel(Cells, specs)
 
 color = p['foci']['foci_plane']
+
+ncc = p['foci']['n_cc']
 
 rep_traces = mm3.extract_foci_array(fov_id_list,Cells_by_peak)
 
@@ -143,8 +145,16 @@ for fov_id in fov_id_list:
         except KeyError:
             pass
 
+for cell_id, cell in Cells.items():
+    cell.initiation_time = None
+    cell.initiation_length = None
+    cell.terminination_time = None
+    cell.initiation_time_n = None
+    cell.initiation_length_n = None
+    cell.C = None
+    cell.D = None
+
 ## link them to cell divisions and calculate cell cycle parameters. color code division line and trace in gui
-ncc = 2
 
 for fov, peaks in fTraces.items():
     for peak, Traces in peaks.items():
@@ -153,43 +163,72 @@ for fov, peaks in fTraces.items():
                 # no overlap of cell cycles
                 cell_ids = np.unique(trace.cell_ids)
                 if len(cell_ids)>1:
-                    print('No overlap set but replication crosses generation')
+                    # print('No overlap set but replication crosses generation')
+                    pass
+
                 else:
                     cell = Cells[cell_id]
                     cell.initiation_time = trace.initiation_time
-                    cell.initiation_size = cell.lengths[np.where(trace.initiation_time == cell.times)]/2**(ncc - 1)
+                    init_i = np.where(trace.initiation_time == cell_m.times)
+                    cell.initiation_length = cell.lengths[init_i]/2**(ncc - 1)*p['pxl2um']
                     cell.termination_time = trace.termination_time
+                    cell.C = (cell.termination_time - cell.initiation_time)*p['min_per_frame']
+                    cell.D = (cell.division_time - cell.termination_time)*p['min_per_frame']
             elif ncc == 2:
                 # initiation occurs in previous generation
                 cell_ids = np.unique(trace.cell_ids)
+                # print(cell_ids)
                 if len(cell_ids) != 2:
-                    print('Found '+str(len(cell_ids)) + ' cells but n_cc is 2')
+                    # print('Found '+str(len(cell_ids)) + ' cells but n_cc is 2')
+                    pass
                 else:
                     cell_m = Cells[cell_ids[0]]
                     cell_d = Cells[cell_ids[1]]
                     cell_d.initiation_time = trace.initiation_time
                     cell_m.initiation_time_n = trace.initiation_time
-                    print(np.where(trace.initiation_time == cell_m.times))
-                    cell_d.initiation_size = np.array(cell_m.lengths)[np.where(trace.initiation_time == cell_m.times)]/2**(ncc - 1)
-                    cell_m.initiation_size_n = np.array(cell_m.lengths)[np.where(trace.initiation_time == cell_m.times)]/2**(ncc - 1)
+                    # print(np.where(trace.initiation_time == cell_m.times))
+                    init_i = np.where(trace.initiation_time == cell_m.times)
+                    cell_d.initiation_length = np.array(cell_m.lengths)[init_i]/2**(ncc - 1) * p['pxl2um']
+                    cell_m.initiation_length_n = np.array(cell_m.lengths)[init_i]/2**(ncc - 1)* p['pxl2um']
                     ## should be able to do these in one line
                     cell_d.termination_time = trace.termination_time
                     cell_m.termination_time_n = trace.termination_time
+                    try:
+                        cell_d.C = (cell_d.termination_time - cell_d.initiation_time)*p['min_per_frame']
+                    except TypeError:
+                        pass
+                    try:
+                        cell_d.D = (cell_d.division_time - cell_d.termination_time)*p['min_per_frame']
+                    except TypeError:
+                        pass
             elif ncc == 3:
                 cell_ids = np.unique(trace.cell_ids)
-                cell_m = cell_ids[0]
-                cell_d = cell_ids[1]
-                cell_gd = cell_ids[2]
-                cell_gd.initiation_time = trace.initiation_time
-                cell_m.initiation_time_n = trace.initiation_time
-                cell_gd.initiation_size = cell_m.lengths[np.where(trace.initiation_time == cell_m.times)]/2**(ncc - 1)
-                cell_m.initiation_size_n = cell_m.lengths[np.where(trace.initiation_time == cell_m.times)]/2**(ncc - 1)
-                ## should be able to do these in one line
-                cell_gd.termination_time = trace.termination_time
-                cell_m.termination_time_n = trace.termination_time
+                if len(cell_ids) != 3:
+                    print('Found '+str(len(cell_ids)) + ' cells but n_cc is 3')
+                    pass
+                else:
+                    cell_m = Cells[cell_ids[0]]
+                    cell_d = Cells[cell_ids[1]]
+                    cell_gd = Cells[cell_ids[2]]
+                    cell_gd.initiation_time = trace.initiation_time
+                    cell_m.initiation_time_n = trace.initiation_time
+                    init_i = np.where(trace.initiation_time == cell_m.times)
+                    cell_gd.initiation_length = cell_m.lengths[init_i]/2**(ncc - 1)* p['pxl2um']
+                    cell_m.initiation_length_n = cell_m.lengths[init_i]/2**(ncc - 1)* p['pxl2um']
+                    ## should be able to do these in one line
+                    cell_gd.termination_time = trace.termination_time
+                    cell_m.termination_time_n = trace.termination_time
+                    cell_gd.C = (cell_gd.termination_time - cell_gd.initiation_time) *p['min_per_frame']
+                    try:
+                        cell_gd.D = (cell_gd.division_time - cell_gd.termination_time)*p['min_per_frame']
+                    except TypeError:
+                        pass
 
 
 
 cell_filename = os.path.basename(cell_file_path)
-with open(os.path.join(p['cell_dir'], cell_filename[:-4] + '_rep_traces.pkl'), 'wb') as trace_file:
+with open(os.path.join(p['cell_dir'], p['experiment_name'] + '_rep_traces.pkl'), 'wb') as trace_file:
     pickle.dump(fTraces, trace_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open(os.path.join(p['cell_dir'], cell_filename[:-4] + '_cc.pkl'), 'wb') as cell_file:
+    pickle.dump(Cells, cell_file, protocol=pickle.HIGHEST_PROTOCOL)
